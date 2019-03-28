b8addec9c295f47b5b7d25ea7088816f322033a8
Shnek <shnek94@gmail.com>
2019-03-28 14:32:03 -0400

    Adding misisng files

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    siteMetadata: {
+        title: `Prywatny Gabinet Psychiatryczny`,
+        subtitle: `Lek. Med. Małgorzata Synowiec`,
+        description: `Strona Prywatnego Gabinetu Psychiatrycznego Lek. Med. Małgorzata Synowiec.`,
+        author: `@shnek`,
+    },
+    pathPrefix: '/pgpms',
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#63ACD5`,
+                theme_color: `#63ACD5`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+    ],
+}
